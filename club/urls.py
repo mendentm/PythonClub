81344e8a1133788c2667c8601a2167a3d2e46c73
Thomas <mendentm@gmail.com>
2021-06-04 21:46:18 -0700

    Meeting View and MeetingDetails views added comit

--- club/urls.py
+++ club/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('resources/', views.resources, name='resources')
+    path('resources/', views.resources, name='resources'),
+    path('meetings/', views.meetings, name='meetings'),
+    path('meetingdetails/<int:id>', views.meetingdetails, name='view'),
 ]
--- club/views.py
+++ club/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
@@ -7,5 +8,12 @@ def index(request):
 
 def resources(request):
     resources_list=Resource.objects.all()
-    return render(request, 'club/resources.html', {'resources_list': resources_list})
+    return render(request, 'club/resources.html', {'resources_list' : resources_list})
 
+def meetings(request):
+    meetings_list=Meeting.objects.all()
+    return render(request, 'club/meetings.html', {'meetings_list' : meetings_list})
+
+def meetingdetails(request, id):    
+    meeting_details_view=get_object_or_404(Meeting, pk=id)
+    return render(request, 'club/meetingdetails.html', {'meeting_details_view': meeting_details_view})
